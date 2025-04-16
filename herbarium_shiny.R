51e7e3a7ce1d2ece36494e867faadc77c37c1119
kcpGOAT <90441353+kcpGOAT@users.noreply.github.com>
2025-04-16 15:44:51 -0700

    Update herbarium_shiny.R

--- herbarium_shiny.R
+++ herbarium_shiny.R
@@ -14,18 +14,22 @@ herb_df <- herbarium_data %>%
          latitude = decimalLatitude,
          longitude = decimalLongitude,
          entered_by = recordEnteredBy) %>%
-  mutate(season = case_when(month %in% c(1:2, 12) ~ "winter",
-                            month %in% 3:5 ~ "spring",
-                            month %in% 6:8 ~ "summer",
-                            month %in% 9:11 ~ "fall")) %>%
+  mutate(season = case_when(month %in% c(1:2, 12) ~ "Winter (December, January, February)",
+                            month %in% 3:5 ~ "Spring (March, April, May)",
+                            month %in% 6:8 ~ "Summer (June, July, August)",
+                            month %in% 9:11 ~ "Fall (September, October, November)"),
+         surname = str_extract(recordedBy, "[:alpha:]+$")) %>%
   filter(!is.na(season), !is.na(longitude), !is.na(latitude), 
-         !is.na(entered_by), str_detect(family, "[:alpha:]"))
+         !is.na(entered_by), str_detect(family, "[:alpha:]")) %>%
+  filter(!(family %in% c("CMP", "LAB")))
 
 ui <- fluidPage(
   titlePanel("The Distribution of UCLA's Botanical Specimen Findings"),
   sidebarLayout(
-    sidebarPanel(actionButton("show_fam", "Include all families?"),
-                 actionButton("clear_fam", "Clear all families?"),
+    sidebarPanel(fluidRow(actionButton("show_fam", "Include all families?"),
+                          actionButton("clear_fam", "Clear all families?")),
+                 fluidRow(actionButton("show_ppl", "Include all collectors?"),
+                          actionButton("clear_ppl", "Clear all collectors?")),
                  checkboxGroupInput("season", "Season of discovery", 
                                     choices = unique(herb_df$season),
                                     selected = unique(herb_df$season)),
@@ -35,6 +39,9 @@ ui <- fluidPage(
                              min = -100, max = 7500, value = c(100, 2000), sep = ""),
                  pickerInput("family", "Family", choices = sort(unique(herb_df$family)),
                              selected = sort(unique(herb_df$family)), multiple = TRUE,
+                             options = pickerOptions(liveSearch = TRUE)),
+                 pickerInput("surname", "Last name of collector", choices = sort(unique(herb_df$surname)),
+                             selected = sort(unique(herb_df$surname)), multiple = TRUE,
                              options = pickerOptions(liveSearch = TRUE))
     ),
     mainPanel(h4("To zoom in, click and drag cursor to create box, then double-click"),
@@ -59,12 +66,20 @@ server <- function(input, output, session) {
     updatePickerInput(session, "family", selected = character(0))
   })
   
-  map_ranges <- reactiveValues(x = c(-125, -112), y = c(30, 45))
+  observeEvent(input$show_ppl, {
+    updatePickerInput(session, "surname", selected = unique(sort(herb_df$family)))
+  })
+  observeEvent(input$clear_ppl, {
+    updatePickerInput(session, "surname", selected = character(0))
+  })
+  
+  map_ranges <- reactiveValues(x = c(-125, -113.5), y = c(30, 42.5))
   
   herb_sf <- reactive({st_as_sf(herb_df[herb_df$season %in% input$season &
-                                          herb_df$year %in% input$year_range[1]:input$year_range[2] &
-                                          herb_df$elevation %in% input$elevation_range[1]:input$elevation_range[2] &
-                                          herb_df$family %in% input$family, ], 
+                                        herb_df$year %in% input$year_range[1]:input$year_range[2] &
+                                        herb_df$elevation %in% input$elevation_range[1]:input$elevation_range[2] &
+                                        herb_df$surname %in% input$surname &
+                                        herb_df$family %in% input$family, ], 
                                 coords = c("longitude", "latitude"), crs = 4326)})
   
   output$map_plot <- renderPlot({
@@ -83,8 +98,8 @@ server <- function(input, output, session) {
       map_ranges$y <- c(brush$ymin, brush$ymax)
       
     } else {
-      map_ranges$x <- c(-125, -112)
-      map_ranges$y <- c(30, 45)
+      map_ranges$x <- c(-125, -113.5)
+      map_ranges$y <- c(30, 42.5)
     }
   })
 }
